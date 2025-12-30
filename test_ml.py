9f931afcdfc46cb95c313bbe915b9b4f8fd384df
KennahS <coffmankennah@gmail.com>
2025-12-30 15:20:55 -0600

    updated imports

--- test_ml.py
+++ test_ml.py
@@ -1,4 +1,6 @@
 import pytest
+import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 # TODO: add necessary import
